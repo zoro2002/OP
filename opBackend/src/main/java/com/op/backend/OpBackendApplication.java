41f8de6ec332f90a88742c8e94705bbc0b3545c9
zoro <proxerM07@gmail.com>
2021-09-15 18:15:44 +0200

    added Backend

---
+++ opBackend/src/main/java/com/op/backend/OpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.op.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpBackendApplication.class, args);
+    }
+
+}
